e09ec0814814fe626de4fe0dd9243ecdcafd84ed
Marie-Therese <ematetelove@gmail.com>
2022-10-14 12:31:46 -0400

    idijpijd

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        for (int i = 0; i < 100; i++) {
+            int randomNum =(int) (Math.random() * 34) + 50 ;
+                    System.out.println(randomNum);
+        }
+
+
+    }
+}
